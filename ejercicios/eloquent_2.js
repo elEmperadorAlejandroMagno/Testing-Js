e01c32c0f1f79e8dca447c2186cf6db2aa5da211
Alejandro M. <alejandromarvich181100@gmai.com>
2024-09-10 16:17:11 -0300

    bean count and objects pag 111

--- ejercicios/eloquent_2.js
+++ ejercicios/eloquent_2.js
@@ -21,3 +21,39 @@ function isEven(n) {
 
 isEven(-3)
 
+
+// bean counting
+
+function countChar(string, ch) {
+  let counting = 0
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() == ch.toLowerCase()) {
+      counting++
+    }
+  }
+  console.log(`The string have ${counting} ${ch}`)
+  return counting
+}
+
+function countBs(string) {
+  let counting = countChar(string, "b")
+  console.log(counting)
+}
+
+countBs("I bought this book in the library")
+console.log(countChar("I like the coconut cream for my face", "c"))
+
+
+//? Objects
+let car = {
+  wheels: 4,
+  windows: 6,
+  doors: 6,
+  horsePower: 281
+}
+
+console.log("wheels" in car)
+// ---> true
+
+console.log("color" in car)
+// ----> false
